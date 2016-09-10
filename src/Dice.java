1ba2fc183148aebed2b387cf5f27b38e3be7fb0e
Nicole Mallory <nmall76@hotmail.com>
2016-09-09 20:38:55 -0400

    Ask user to roll dice

--- src/Dice.java
+++ src/Dice.java
@@ -3,19 +3,27 @@ import java.util.Scanner;
 public class Dice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Ask the user to enter the number
+	
+		
 		int number = 0;
+		String yes ="";
 		
 		//Create scanner
 		Scanner input = new Scanner(System.in);
+		
+		//Ask the user if they want to roll the dice
+		System.out.println("Welcome to the Grand Circus Casino! Roll the dice? (y/n)");
+		yes = input.next();
+			if (yes.equalsIgnoreCase("yes")){
+		}
+		//Ask the user to enter the number
+		System.out.println("Enter number of sides to a pair of dice:");
+		number = input.nextInt();
+		
+		//Prompt the user to roll the dice
+		System.out.println("Let's roll the dice:");
 	
 		
-		System.out.println("Enter number of side to a pair of dice:");
-		number = input.nextInt();
-			
-		System.out.println("Welcome to the Grand Circus Casino!");
-		//prompt the user to roll the dice
 		//Display the result of teach of them
 		//Ask them if she wants to roll the dice again
 
