330073a35267189f366c6836c2bd93d1f5b9a91b
Nicole Mallory <nmall76@hotmail.com>
2016-09-09 20:12:44 -0400

    Initial commit

---
+++ src/Dice.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Dice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Ask the user to enter the number
+		int number = 0;
+		
+		//Create scanner
+		Scanner input = new Scanner(System.in);
+	
+		
+		System.out.println("Enter number of side to a pair of dice:");
+		number = input.nextInt();
+			
+		System.out.println("Welcome to the Grand Circus Casino!");
+		//prompt the user to roll the dice
+		//Display the result of teach of them
+		//Ask them if she wants to roll the dice again
+
+	}
+}
